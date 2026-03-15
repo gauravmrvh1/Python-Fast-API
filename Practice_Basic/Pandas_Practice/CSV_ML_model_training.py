4570a071aed73f92df5fc415f56554af1e43a77d
gaurav.marvaha <gaurav.marvaha@daffodilsw.com>
2026-03-15 17:02:03 +0530

    machine learning added

--- Practice_Basic/Pandas_Practice/CSV_ML_model_training.py
+++ Practice_Basic/Pandas_Practice/CSV_ML_model_training.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("students.csv")
-
 print(df.head())
 
 
@@ -22,7 +21,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Model Train
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 
 # Prediction
 new_data = pd.DataFrame({
